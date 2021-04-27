75db542ea3db8101772586eabe3dfa0fc062355b
omar-ahmd <omaharmad@gmail.com>
2021-04-27 14:03:43 +0300

    fix 0x01 task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    str = "".join([chr(ord(i) - 32) if ord(i) >= ord("a") and ord(i) <= ord("z") else i for i in str])
+    str = "".join([chr(ord(i) - 32) if (ord(i) >= ord("a") and
+                                        ord(i) <= ord("z")) else i
+                   for i in str])
     print(str)
