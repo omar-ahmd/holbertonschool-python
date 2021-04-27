5e9b1f1eb932ff9a1e16b75845c4430cfdc8980b
omar-ahmd <omaharmad@gmail.com>
2021-04-27 12:38:35 +0300

    fix 0x01 task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for x in range(97, 123):
-    print('{}'.format(chr(x)), end="") if (chr(x) != 'q' and chr(x) != 'e')
+    if chr(x) != "q" and chr(x) != "e":
+        print('{}'.format(chr(x)), end="")
