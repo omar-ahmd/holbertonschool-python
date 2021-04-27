eb21a7608a21d2336c3faa7f1f95a994c63bf47e
omar-ahmd <omaharmad@gmail.com>
2021-04-27 12:30:15 +0300

    0x01 task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,14 +1,3 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-lstdig = abs(number) % 10
-result = "Last digit of {:d} is {:d}".format(number,
-                                             lstdig if number > 0
-                                             else -(lstdig))
-if lstdig > 5:
-    result += " and is greater than 5"
-elif lstdig == 0:
-    result += " and is 0"
-else:
-    result += " and is less than 6 and not 0"
-print(result)
+ #!/usr/bin/python3
+for x in range(97, 123):
+    print('{}'.format(chr(x)), end="")
