b2d0cb4e3513618a6f93a7b0cdfd52461142cff9
omar-ahmd <omaharmad@gmail.com>
2021-04-27 14:06:04 +0300

    fix 0x01 task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     for x in str:
         flag = 0
         if ord(x) >= ord("a") and ord(x) <= ord("z"):
-            print('{:c}'.format(ord(x) -32), end="")
-        else:
-            print('{:c}'.format(ord(x)), end="")
+            flag = 32
+        print('{:c}'.format(ord(x) - flag), end="")
     print()
