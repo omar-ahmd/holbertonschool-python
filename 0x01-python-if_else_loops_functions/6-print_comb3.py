a0167d00e8eac8633cbd54bdf334e682a07d3001
omar-ahmd <omaharmad@gmail.com>
2021-04-27 12:59:09 +0300

    0x01 task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for x in range(0, 100):
+    if int(x / 10) != x % 10 and  ((x%10)*10+x/10) > x:
+        if x < 89:
+            print('{:02d}'.format(int(x)), end=", ")
+        else:
+            print('{:02d}'.format(int(x)))
