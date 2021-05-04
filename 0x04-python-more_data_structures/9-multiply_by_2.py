b969769301ade57cb50284ccd68d664050a42a27
omar-ahmd <omaharmad@gmail.com>
2021-05-04 20:27:54 +0300

    fix 0x04 task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary_copy.keys():
-        a_dictionary[key] = a_dictionary[key] * 2
-    return a_dictionary
+    new_a_dictionary = dict(a_dictionary)
+    for key in new_a_dictionary.keys():
+        new_a_dictionary[key] = new_a_dictionary[key] * 2
+    return new_a_dictionary
