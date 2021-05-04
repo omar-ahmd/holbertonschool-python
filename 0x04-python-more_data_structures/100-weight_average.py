170601f3aad67ad1f8e4c63011093056c3e144a2
omar-ahmd <omaharmad@gmail.com>
2021-05-04 22:01:56 +0300

    fix 0x04 task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,8 +4,8 @@ def weight_average(my_list=[]):
         return 0
     else:
         sum = 0
-        aver = 0 
+        aver = 0
         for element in my_list:
             aver += element[0] * element[1]
             sum += element[1]
-        return aver/sum 
+        return aver/sum
