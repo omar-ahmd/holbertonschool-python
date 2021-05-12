a2179d075cf0ca1536ac964f92df4fb3b622cd10
omar-ahmd <omaharmad@gmail.com>
2021-05-12 13:15:59 +0300

    0x06 task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+ #!/usr/bin/python3
+"""
+    module contains a class with a private instance size
+"""
+
+
+class Square:
+    """
+       Square class
+    """
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
