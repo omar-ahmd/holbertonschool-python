a61a917478fb97fedd45fbda7481c046b0b2ce6d
omar-ahmd <omaharmad@gmail.com>
2021-05-12 13:31:26 +0300

    0x06 task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    module contains a class with a private instance size
+"""
+
+
+class Square:
+    """
+       Square class
+    """
+    def __init__(self, size=0):
+        """Initializes the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area of the square"""
+        return size**2
