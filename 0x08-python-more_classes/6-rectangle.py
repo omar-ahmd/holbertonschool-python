4759c791da3b343623dea37fb3139c9072b0c428
omar-ahmd <omaharmad@gmail.com>
2021-05-18 17:33:52 +0300

    fix 0x08 task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
        Rectangle class
     """
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         """Initialize the rectangle class"""
         self.width = width
