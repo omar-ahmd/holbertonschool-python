8a367d7622c9eb5256b1d18c6e426f5d13fb0b4a
omar-ahmd <omaharmad@gmail.com>
2021-05-26 18:05:53 +0300

    fix 0X0B task4

--- 0x0B-python-inheritance/4-inherits_from.py
+++ 0x0B-python-inheritance/4-inherits_from.py
@@ -6,5 +6,5 @@ inherits_from Module
 
 
 def inherits_from(obj, a_class):
-    """Returns True if the object is an instance of the class that inherited from"""
+    """True if the object is an instance of the class that inherited from"""
     return isinstance(obj, a_class) and type(obj) != a_class
